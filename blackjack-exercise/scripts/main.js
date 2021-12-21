664a7764a58462a6658d7e251fb96b3b334a5e64
christycrook <92058140+christycrook@users.noreply.github.com>
2021-12-21 00:26:02 -0600

    changed code

--- blackjack-exercise/scripts/main.js
+++ blackjack-exercise/scripts/main.js
@@ -1,53 +1,106 @@
-window.addEventListener('DOMContentLoaded', function() {
-  function deal(deck, target) {
-    const currentCard = deck.pop();
-    target.push(currentCard);
-  }
-  
-  function getCardImage(card) {
-    const cardImage = document.createElement('img');
-    if (card.rank === 1) { // 1 = ace
-      cardImage.setAttribute('src', `images/ace_of_${card.suit}.png`);
-    } else if (card.rank === 11) { // 11 = jack
-      cardImage.setAttribute('src', `images/jack_of_${card.suit}.png`);
-    } else if (card.rank === 12) { // 12 = queen
-      cardImage.setAttribute('src', `images/queen_of_${card.suit}.png`);
-    } else if (card.rank === 13) { // 13 = king
-      cardImage.setAttribute('src', `images/king_of_${card.suit}.png`);
-    } else { // otherwise, it's a number card
-      cardImage.setAttribute('src', `images/${card.rank}_of_${card.suit}.png`);
+function deal(deck, target) {
+  const currentCard = deck.pop();
+  target.push(currentCard);
+}
+
+function buildDeck() {
+  let deck = [];
+  let suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+  let rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < rank.length; j++) {
+      let card = {rank: rank[j], suit: suits[i]};
+      deck.push(card);
     }
-    return cardImage
   }
-  
-  function render(isGameOver) {
-    const dealerHandElement = document.querySelector('#dealer-hand');
-    const playerHandElement = document.querySelector('#player-hand');
-    dealerHandElement.innerHTML = ''
-    playerHandElement.innerHTML = ''
-  
-    // for each card in the dealer hand
-    for (let index = 0; index < dealerHand.length; index++) {
-      const card = dealerHand[index];
+  shuffle(deck);
+  return deck;
+}
+
+  let isGameOver = false;
+  let dealerHand = [];
+  let playerHand = [];
+  let gameDeck = buildDeck;
+
+  const dealButton = document.querySelector('#deal-button');
+  dealButton.addEventListener('click', function() {
+    dealButton.setAttribute('disabled', true);
+    dealPlayerHand();
+    dealDealerHand();
+  })
+
+  const hitButton = document.querySelector('#hit-button');
+  hitButton.addEventListener('click', function() {
+    dealPlayerHand();
+    checkBusts();
+    renderDealer(isGameOver);
+    renderPlayer(isGameOver);
+  })
+
+  const standButton = document.querySelector('#stand-button');
+  standButton.addEventListener('click', function() {
+    let dealerPoints = calculatePoints(dealerHand);
+    while (dealerPoints < 17) {
+      dealDealerHand();
+      dealerPoints = calculatePoints(dealerHand);
+    }
+    checkWinner();
+    renderDealer(isGameOver);
+    renderPlayer(isGameOver);
+  })
+
+  function dealDealerHand() {
+    const currentCard = shuffle(deck);
+    dealerHand.push(currentCard)
+  } 
+
+  function dealPlayerHand() {
+    const currentCard = shuffle(deck);
+    playerHand.push(currentCard)
+  }
+
+  let deck = buildDeck();
+  function dealDeck() {
+    let dealerHand = [];
+    let playerHand = [];
+    shuffle(deck);
+
+    for (let i = 0; i < dealerHand.length; i++) {
+      const card = dealerHand[i];
       const cardElement = getCardImage(card)
       dealerHandElement.appendChild(cardElement);
     }
-    // do the same for the player hand
-    for (let index = 0; index < playerHand.length; index++) {
-      const card = playerHand[index];
+
+    for (let i = 0; i < playerHand.length; i++) {
+      const card = playerHand[i];
       const cardElement = getCardImage(card)
       playerHandElement.appendChild(cardElement);
     }
-  
-    // calculate the player points
-    const playerPoints = calculatePoints(playerHand);
-    document.querySelector('#player-points').textContent = playerPoints;
+  }
+
+  function getCardImage(card) {
+    const cardImage = document.createElement('img');
+    if (card.rank === 1) {
+      cardImage.setAttribute('src', `../images/ace_of_${card.suit}.png`);
+    } else if (card.rank === 11) {
+      cardImage.setAttribute('src', `../images/jack_of_${card.suit}.png`);
+    } else if (card.rank === 12) {
+      cardImage.setAttribute('src', `../images/queen_of_${card.suit}.png`);
+    } else if (card.rank === 13) {
+      cardImage.setAttribute('src', `../images/king_of_${card.suit}.png`);
+    } else {
+      cardImage.setAttribute('src', `../images/${card.rank}_of_${card.suit}.png`);
+    }
+    return cardImage
+  }
+
+  function renderDealer(isGameOver) {
+    const dealerHandElement = document.querySelector('#dealer-hand');
+    dealerHandElement.innerHTML = ''
     
-    // do the same for the dealer
     const dealerPoints = calculatePoints(dealerHand);
     document.querySelector('#dealer-points').textContent = dealerPoints;
   
-    // if we told the render function that the game is over
     if (isGameOver) {
       const hitButton = document.querySelector('#hit-button');
       const standButton = document.querySelector('#stand-button');
@@ -55,48 +108,36 @@ window.addEventListener('DOMContentLoaded', function() {
       standButton.setAttribute('disabled', true);
     }
   }
-  
-  function generateDeck() {
-    let deck = [];
-  
-    // set up our suits
-    const suits = [
-      'clubs',
-      'spades',
-      'hearts',
-      'diamonds'
-    ]
-  
-    // for each suit
-    for (let suitIndex = 0; suitIndex < suits.length; suitIndex++) {
-      const suit = suits[suitIndex];
-      for (let rank = 1; rank <= 13; rank++) {
-        const card = {
-          rank: rank,
-          suit: suit,
-        };
-        deck.push(card);
-      }
-    }
-    // shuffle the deck array
-    shuffle(deck)
-    return deck;
+
+  function renderPlayer(isGameOver) {
+    const playerHandElement = document.querySelector('#player-hand');
+    playerHandElement.innerHTML = ''
+
+    const playerPoints = calculatePoints(playerHand);
+    document.querySelector('#player-points').textContent = playerPoints;
+
+    if (isGameOver) {
+      const hitButton = document.querySelector('#hit-button');
+      const standButton = document.querySelector('#stand-button');
+      hitButton.setAttribute('disabled', true);
+      standButton.setAttribute('disabled', true);
+    }    
   }
-  
+
   function shuffle(deck) {
     for (let i = 0; i < deck.length; i++) {
-      const randIndex = Math.floor(Math.random() * i);
+      const randIndex = Math.floor(Math.random() * deck.length);
       const temporary = deck[i]
       deck[i] = deck[randIndex];
       deck[randIndex] = temporary;
     }
+    return;
   }
-  
+
   function calculatePoints(deck) {
     let points = 0;
     let aces = 0;
   
-    // for each card in the hand
     for (let i = 0; i < deck.length; i++) {
       const currentCard = deck[i];
   
@@ -110,19 +151,17 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
   
-    // for as long as the points are > 21 (bust) and as long as we have aces to count
     while (points > 21 && aces > 0) {
       points -= 10;
       aces--;
     }
     return points;
   }
-  
+
   function checkBusts() {
     const playerPoints = calculatePoints(playerHand);
     const dealerPoints = calculatePoints(dealerHand);
   
-    // check busts (points > 21)
     if (playerPoints > 21) {
       message('Player Bust. Dealer Wins');
       isGameOver = true;
@@ -131,12 +170,11 @@ window.addEventListener('DOMContentLoaded', function() {
       isGameOver = true;
     }
   }
-  
+
   function checkWinner() {
     const playerPoints = calculatePoints(playerHand);
     const dealerPoints = calculatePoints(dealerHand);
     
-    // check points (higher value)
     if (dealerPoints >= 17 && playerPoints >= dealerPoints && playerPoints >= 21) {
       message('Player Wins')
       isGameOver = true;
@@ -151,63 +189,17 @@ window.addEventListener('DOMContentLoaded', function() {
       isGameOver = true;
     }
   }
-  
+
   function message(text) {
     document.querySelector('#messages').textContent = text
   }
-
-  let isGameOver = false;
-  let dealerHand = [];
-  let playerHand = [];
-  let gameDeck = generateDeck();
-
-  // find the deal button
-  const dealButton = document.querySelector('#deal-button');
-  dealButton.addEventListener('click', function() {
-    // disable the deal button
-    dealButton.setAttribute('disabled', true);
-    // deal to the dealer and the player
-    deal(gameDeck, playerHand);
-    deal(gameDeck, dealerHand);
-    deal(gameDeck, playerHand);
-    deal(gameDeck, dealerHand);
     
-    // calculate the points
-    const playerPoints = calculatePoints(playerHand);
-    const dealerPoints = calculatePoints(dealerHand);
+  const playerPoints = calculatePoints(playerHand);
+  const dealerPoints = calculatePoints(dealerHand);
 
-    // player has 21
-    if (playerPoints === 21 && dealerPoints < 21) {
-      message('Player has 21. Player wins');
-      isGameOver = true;
-    }
-    render(isGameOver);
-  })
-
-  // find the hit button
-  const hitButton = document.querySelector('#hit-button');
-  hitButton.addEventListener('click', function() {
-    // deal to the player
-    deal(gameDeck, playerHand);
-    // check if anyone busted
-    checkBusts();
-    render(isGameOver);
-  })
-
-  // find the stand button
-  const standButton = document.querySelector('#stand-button');
-  standButton.addEventListener('click', function() {
-    let dealerPoints = calculatePoints(dealerHand);
-    // As long as the dealer has less than 17
-    while (dealerPoints < 17) {
-      deal(gameDeck, dealerHand);
-      // update dealer points
-      dealerPoints = calculatePoints(dealerHand);
-    }
-    // check if anyone has won
-    checkWinner();
-    render(isGameOver);
-  })
-
-  
-})
+  if (playerPoints === 21 && dealerPoints < 21) {
+    message('Player has 21. Player wins');
+    isGameOver = true;
+  }
+  renderDealer(isGameOver);
+  renderPlayer(isGameOver);
